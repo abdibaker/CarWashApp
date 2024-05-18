99d58017b1142ecad03d050e368582a977a3329b
Abdillah Bakari <abdibaker1@gmail.com>
2024-05-18 07:21:50 +0300

    feat: Update TabLayout to include logo and avatar images
    The TabLayout component in _layout.tsx was updated to include a logo image and an avatar image in the header. This improves the visual appeal of the layout and provides branding elements for the app.

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -1,6 +1,6 @@
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
+import { Image, View } from 'react-native';
 
-import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
 
 export default function TabLayout() {
@@ -8,17 +8,28 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: 'black',
+        headerLeft: () => (
+          <View className="bg-red-700">
+            <Image
+              source={require('@/assets/logo.png')}
+              style={{ height: 40, width: 80, margin: 10 }} // Removed the 'objectFit' property
+            />
+          </View>
+        ),
+        headerRight: () => (
+          <View className="bg-red-700">
+            <Image
+              source={require('@/assets/avatar.png')}
+              style={{ height: 40, width: 40, margin: 10 }} // Removed the 'objectFit' property
+            />
+          </View>
+        ),
       }}>
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
+          title: '',
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <HeaderButton />
-          //   </Link>
-          // ),
         }}
       />
       <Tabs.Screen
